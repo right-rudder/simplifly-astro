d52092a2e426d35b75966a8d3f6e977712c54032
Raul Ospina <raminka13@hotmail.com>
2025-01-24 16:45:39 -0500

    New: pilot made simple page

---
+++ src/data/pilot-training-made-simple.js
@@ -0,0 +1,45 @@
+const pilotTrainingMadeSimple = {
+  data: {
+    //SEO Header Stuff
+    pageDescription:
+      "SimpliFly Flight School & Aircraft Management offers flight training in Kankakee, IL. We offer private pilot training, instrument rating training, commercial pilot training, and more. Contact us today!",
+    pageKeywords:
+      "flight training, flight school, pilot training, private pilot training, instrument rating training, commercial pilot training, Kankakee, IL",
+
+    //top header
+    header: {
+      stars: true,
+      imagePath:
+        "/src/assets/SimpliFly-flight-school-airplane-landing-at-falcon-field-airport-pilot-training-mesa-arizona.webp",
+      imageAlt:
+        "SimpliFly Flight School airplane landing at Falcon Field Airport in Mesa, Arizona",
+      headerH1: `PILOT TRAINING</br><span class="text-sky-300">MADE SIMPLE</span>`,
+      paragraph:
+        "How would you like to learn to fly? At SimpliFly, we make it easy to get started. Our flight school offers a variety of pilot training programs to help you achieve your aviation goals.",
+      buttons: [
+        {
+          name: "Enroll Now",
+          link: "/enroll-now",
+          primary: false,
+        },
+      ],
+    },
+
+    pageSummary: {
+      title: "Where Do I Start?",
+      paragraphs: [
+        "Learning to fly can be a daunting task for beginners. It’s like learning a whole new language, and the path to success might not always be clear. But don’t worry, we’ve got you covered at SimpliFly Flight School!",
+        "We’ve designed our training program to be efficient and easy to understand. Keep reading to learn more about our flight training schedules, how we progress students, and what we expect from our career track students.",
+      ],
+      buttons: [
+        {
+          name: "Book a Discovery Flight",
+          link: "/discovery-flight",
+          primary: false,
+        },
+      ],
+    },
+  },
+};
+
+export default pilotTrainingMadeSimple;
