3a709c0d65029f5baf7038f3ec731ce59b494da2
lucascr <lcrodrigues1995@gmail.com>
2025-02-20 20:23:16 -0300

    Create coursesIndex.js

---
+++ src/data/coursesIndex.js
@@ -0,0 +1,36 @@
+import { COMPANY_NAME } from "../consts";
+
+const coursesIndex = {
+  data: {
+    // SEO Header Stuff
+    pageTitle: `Training Programs | ${COMPANY_NAME}`,
+    pageDescription:
+      "Discover SimpliFly Flight School’s comprehensive pilot training programs. From Private Pilot License to advanced certifications like Instrument Rating, Multi-Engine, and Certified Flight Instructor, our expert-led courses are designed to help you achieve your aviation dreams.",
+    pageKeywords:
+      "flight training courses Arizona, aviation certifications, private to commercial pilot, multi-engine rating training, CFI courses Mesa, aviation school programs",
+
+    // Top Header
+    header: {
+      stars: true,
+      imagePath: "/src/assets/airline-pilot-courses-at-simplifly-flight-school.webp",
+      imageAlt:
+        "Photograph of a SimpliFly Flight School aircraft soaring over the Arizona landscape",
+      headerH1: `Pilot <br>PROGRAMS</span>`,
+      paragraph: `Join SimpliFly Flight School and take your first step towards a successful aviation career. Whether you're starting with a discovery flight or pursuing advanced ratings, we’re here to support you every step of the way.`,
+      buttons: [
+        {
+          name: "Enroll Today",
+          link: "/enroll-now",
+          primary: false,
+        },
+        {
+          name: "Program Guide",
+          link: "/path",
+          primary: false,
+        },
+      ],
+    },
+  },
+};
+
+export default coursesIndex;
