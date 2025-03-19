3976f5f39d44a870bce9257dc26b6668653bd9db
Raul Ospina <raminka13@hotmail.com>
2025-03-19 15:04:20 -0500

    New: begin pilot career page

---
+++ src/data/begin-pilot-career.js
@@ -0,0 +1,120 @@
+const beginPilotCareer = {
+  data: {
+    //SEO Header Stuff
+    pageTitle: "Start Your Pilot Career || SimpliFly Flight School, Mesa, AZ",
+    pageDescription:
+      "Begin your pilot career as a certified flight instructor (CFI) at SimpliFly Flight School. We offer a 30-day CFI program to help you build the necessary hours to become an airline pilot. Contact us today!",
+    pageKeywords:
+      "flight training, flight training Mesa, flight training Arizona, flight school, flight school Mesa, flight school Arizona, pilot training, CFI training, certified flight instructor, pilot career, airline pilot, Mesa, AZ, Arizona",
+
+    //top header
+    header: {
+      stars: true,
+      imagePath:
+        "/src/assets/airline-pilot-courses-at-simplifly-flight-school.webp",
+      imageAlt:
+        "SimpliFly Flight School airplane landing at Falcon Field Airport in Mesa, Arizona",
+      headerH1: `Career Track<br>Pilots`,
+      paragraph:
+        "Start your pilot career after getting your commercial certificate. Start flying now and build the necessary hours to become an airline pilot. One way SimpliFly can help is by becoming a certified flight instructor (CFI).",
+      buttons: [
+        {
+          name: "Enroll Now",
+          link: "/enroll-at-simplifly",
+          primary: false,
+        },
+      ],
+    },
+
+    pageSummary: {
+      title: "From Zero to Pilot",
+      paragraphs: [
+        "Start your pilot career after getting your commercial certificate. Start flying now and build the necessary hours to become an airline pilot. One way SimpliFly can help is by becoming a certified flight instructor (CFI).",
+        "Get all required licenses and ratings, log flight hours, and pass the necessary tests to become a CFI. SimpliFly will guide you through the process and help you achieve your aviation career goals.",
+        "Become a pilot and start your journey towards a rewarding career in aviation. Contact SimpliFly today to learn more about our pilot training programs and take the first step towards achieving your aviation career goals!",
+      ],
+      buttons: [
+        {
+          name: "Pilot Timeline",
+          link: "/timeline-to-be-a-pilot",
+          primary: true,
+        },
+      ],
+    },
+
+    contentSplit: {
+      upperHeading: "Get Started",
+      heading: "Your Pilot Career",
+      descriptions: [
+        "Start your pilot career after getting your commercial certificate. Start flying now and build the necessary hours to become an airline pilot. One way SimpliFly can help is by becoming a certified flight instructor (CFI).",
+        "Being a CFI offers a valuable opportunity to rapidly accumulate flight experience for the minimum requirements of airlines and corporate aviation companies. It is a great opportunity that allows you to apply all the knowledge and skills acquired during your own flight training.",
+        "Flight instructors play a crucial role as ambassadors of the aviation community, imparting their knowledge and experience to their students. For many, it serves as their first aviation job that not only is an invaluable experience but also provides compensation.",
+      ],
+      bulletsTitle: "What are the Eligibility Requirements?",
+      bullets: [
+        {
+          title: "Be 18 years of age.",
+          descriptions: ["To become a CFI, you must be at least 18 years old."],
+          icon: "M9 12.75 11.25 15 15 9.75m-3-7.036A11.959 11.959 0 0 1 3.598 6 11.99 11.99 0 0 0 3 9.749c0 5.592 3.824 10.29 9 11.623 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.571-.598-3.751h-.152c-3.196 0-6.1-1.248-8.25-3.285Z",
+        },
+        {
+          title: "English Proficiency",
+          descriptions: [
+            "Read, speak, write and understand the English language.",
+          ],
+          icon: "M9 12.75 11.25 15 15 9.75m-3-7.036A11.959 11.959 0 0 1 3.598 6 11.99 11.99 0 0 0 3 9.749c0 5.592 3.824 10.29 9 11.623 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.571-.598-3.751h-.152c-3.196 0-6.1-1.248-8.25-3.285Z",
+        },
+        {
+          title: "Licenses & Ratings",
+          descriptions: [
+            "Hold a commercial pilot certificate and an instrument rating.",
+          ],
+          icon: "M9 12.75 11.25 15 15 9.75m-3-7.036A11.959 11.959 0 0 1 3.598 6 11.99 11.99 0 0 0 3 9.749c0 5.592 3.824 10.29 9 11.623 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.571-.598-3.751h-.152c-3.196 0-6.1-1.248-8.25-3.285Z",
+        },
+        {
+          title: "Flight Hours",
+          descriptions: ["Have logged at least 250 hours of flight time."],
+          icon: "M9 12.75 11.25 15 15 9.75m-3-7.036A11.959 11.959 0 0 1 3.598 6 11.99 11.99 0 0 0 3 9.749c0 5.592 3.824 10.29 9 11.623 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.571-.598-3.751h-.152c-3.196 0-6.1-1.248-8.25-3.285Z",
+        },
+        {
+          title: "Medical Certificate",
+          descriptions: [
+            "Hold a valid FAA 3rd Class Medical Certificate (or higher)",
+          ],
+          icon: "M9 12.75 11.25 15 15 9.75m-3-7.036A11.959 11.959 0 0 1 3.598 6 11.99 11.99 0 0 0 3 9.749c0 5.592 3.824 10.29 9 11.623 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.571-.598-3.751h-.152c-3.196 0-6.1-1.248-8.25-3.285Z",
+        },
+        {
+          title: "Fundamentals of Instructing",
+          descriptions: [
+            "Receive a logbook endorsement from an authorized instructor on the fundamentals of instructing.",
+          ],
+          icon: "M9 12.75 11.25 15 15 9.75m-3-7.036A11.959 11.959 0 0 1 3.598 6 11.99 11.99 0 0 0 3 9.749c0 5.592 3.824 10.29 9 11.623 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.571-.598-3.751h-.152c-3.196 0-6.1-1.248-8.25-3.285Z",
+        },
+        {
+          title: "Knowledge Test",
+          descriptions: [
+            "Pass a knowledge test on the aeronautical knowledge areas appropriate to the flight instructor rating sought.",
+          ],
+          icon: "M9 12.75 11.25 15 15 9.75m-3-7.036A11.959 11.959 0 0 1 3.598 6 11.99 11.99 0 0 0 3 9.749c0 5.592 3.824 10.29 9 11.623 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.571-.598-3.751h-.152c-3.196 0-6.1-1.248-8.25-3.285Z",
+        },
+      ],
+      finalSection: {
+        heading: "How Long Will it Take?",
+        descriptions: [
+          "Becoming a flight instructor does not have a minimum time, but you do need a commercial pilot license along with an instrument rating. To qualify for a commercial pilot license, a pilot must accumulate a minimum of 250 flight hours. If studying part time when able (flexible), it can take from a few weeks to a few months to be prepared for the CFI checkride and receive your endorsement.",
+          "We offer an accelerated 30 day course because that is the fastest timeline. It takes at least 30 days if you are full time and accelerated. In this program, you will be working alongside pilots at a similar stage in their careers that are also working on their CFI certificates. Click the button below to get more information and start the enrollment process.",
+        ],
+      },
+      finalButton: {
+        name: "Enroll at SimpliFly",
+        link: "/enroll-at-simplifly",
+        primary: true,
+      },
+      imagePath: "/src/assets/female_pilot_smiling.webp",
+      imageAlt:
+        "sun city aviation founder luke poulos in front of a sun city airplane",
+    },
+  },
+};
+
+export default beginPilotCareer;
