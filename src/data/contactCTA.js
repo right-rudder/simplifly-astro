3bbc2ea91b91b9b0ee86729b036c0176434c545e
lucascr <lcrodrigues1995@gmail.com>
2025-04-14 16:11:11 -0300

    Create contactCTA.js

---
+++ src/data/contactCTA.js
@@ -0,0 +1,13 @@
+const contactCTA = {
+  imagePath: "/src/assets/pilot_thumbs-cropped.webp",
+  imageAlt: "SimpliFly Flight School student flying over Arizona",
+  headerH1: `Still have questions?<br/>Contact us so we can help!`,
+  buttons: [
+    {
+      name: "Contact Us",
+      link: "/contact",
+      primary: true,
+    },
+  ],
+};
+export default contactCTA;
