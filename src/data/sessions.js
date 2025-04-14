8a1bc4bf9c2b5ff470e4eabd053d131a826a1115
lucascr <lcrodrigues1995@gmail.com>
2025-04-14 15:31:00 -0300

    Create sessions.js

---
+++ src/data/sessions.js
@@ -0,0 +1,52 @@
+const groundSchoolSessions = [
+  {
+    date: "April 19, 2025",
+    time: "8:00 AM – 11:00 AM",
+    topics: ["Pilot Qualifications", "Airworthiness"],
+    status: "Scheduled",
+  },
+  {
+    date: "May 10, 2025",
+    time: "8:00 AM – 11:00 AM",
+    topics: ["Radio Communications", "Traffic Patterns"],
+    status: "Scheduled",
+  },
+  {
+    date: "May 31, 2025",
+    time: "8:00 AM – 11:00 AM",
+    topics: ["Weather", "Airspace"],
+    status: "Scheduled",
+  },
+  {
+    date: "June 21, 2025",
+    time: "8:00 AM – 11:00 AM",
+    topics: ["Performance Limitations"],
+    status: "Scheduled",
+  },
+  {
+    date: "TBD",
+    time: "TBD",
+    topics: ["Systems / POH"],
+    status: "Coming Soon",
+  },
+  {
+    date: "TBD",
+    time: "TBD",
+    topics: ["Human Factors"],
+    status: "Coming Soon",
+  },
+  {
+    date: "TBD",
+    time: "TBD",
+    topics: ["Cross Country"],
+    status: "Coming Soon",
+  },
+  {
+    date: "TBD",
+    time: "TBD",
+    topics: ["Airman Certification Standards (ACS) Book"],
+    status: "Coming Soon",
+  },
+];
+
+export default groundSchoolSessions;
