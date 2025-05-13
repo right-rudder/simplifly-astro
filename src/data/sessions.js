b03f94717a40b3d939d413c925f312882fef7a20
lucascr <lcrodrigues1995@gmail.com>
2025-05-13 15:31:42 -0300

    Update sessions.js

--- src/data/sessions.js
+++ src/data/sessions.js
@@ -13,13 +13,13 @@ const groundSchoolSessions = [
   },
   {
     date: "May 31, 2025",
-    time: "8:00 AM – 11:00 AM",
+    time: "9:00 AM - 12:00 PM",
     topics: ["Weather", "Airspace"],
     status: "Scheduled",
   },
   {
     date: "June 21, 2025",
-    time: "8:00 AM – 11:00 AM",
+    time: "9:00 AM - 12:00 PM",
     topics: ["Performance Limitations"],
     status: "Scheduled",
   },
