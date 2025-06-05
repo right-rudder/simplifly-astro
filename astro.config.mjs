dae59c898e3e7ca3d926f36695ede99c538c7854
Raul Ospina <raminka13@hotmail.com>
2025-06-05 11:18:43 -0500

    Remove Partytown integration and update dependencies in configuration files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import partytown from "@astrojs/partytown";
 import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://simpliflyco.com/",
-  integrations: [tailwind(), partytown(), sitemap(), react()],
+  integrations: [tailwind(), sitemap(), react()],
   redirects: {
     "/posts/[...slug]": "/blog/[...slug]",
     "/our-team": "/simplifly/team",
